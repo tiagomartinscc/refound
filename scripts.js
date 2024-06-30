3ba934b0faacb21c41dc51cb80866cd58628c155
Tiago Martins <tiagomartinscc@hotmail.com>
2024-06-29 22:29:15 -0300

    add format currency on input

--- scripts.js
+++ scripts.js
@@ -1,6 +1,20 @@
 // seleciona os elementos do formulario
 
 const amount = document.getElementById('amount')
+
+// capturando o input para formatar o valor
 amount.oninput = () => {
-  console.log(amount.value)
+  // remove caracteres nao numericos
+  let value = amount.value.replace(/\D+/g, '')
+
+  // converte para centavos
+  value = Number(value) / 100
+  amount.value = formatCurrencyBRL(value)
+}
+
+function formatCurrencyBRL(value) {
+  return value.toLocaleString("pt-BR", {
+    style: 'currency',
+    currency: 'BRL'
+  })
 }
