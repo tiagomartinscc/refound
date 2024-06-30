46ece065ed003b49b962076836f9f66cf12d39a6
Tiago Martins <tiagomartinscc@hotmail.com>
2024-06-29 23:18:28 -0300

    add info to list

--- scripts.js
+++ scripts.js
@@ -5,6 +5,9 @@ const amount = document.getElementById('amount')
 const expense = document.getElementById('expense')
 const category = document.getElementById('category')
 
+// elementos da lista
+const expenseList = document.querySelector('ul')
+
 // capturando o input para formatar o valor
 amount.oninput = () => {
   // remove caracteres nao numericos
@@ -35,4 +38,54 @@ form.onsubmit = (event) => {
     created_at: new Date()
   }
 
+  expenseAdd(newExpense)
+}
+
+function expenseAdd(newExpense) {
+  try {
+    // Cria o elemento para a adicionar o item (li) na lista (ul)
+    const expenseItem = document.createElement("li")
+    expenseItem.classList.add("expense")
+
+    // Cria o ícone da categoria
+    const expenseIcon = document.createElement("img")
+    expenseIcon.setAttribute("src", `img/${newExpense.category_id}.svg`)
+    expenseIcon.setAttribute("alt", newExpense.category_name)
+
+    // Cria a info da despesa
+    const expenseInfo = document.createElement("div")
+    expenseInfo.classList.add("expense-info")
+
+    // Cria o nome da despesa
+    const expenseName = document.createElement("strong")
+    expenseName.textContent = newExpense.expense
+
+    // Cria a categoria da despesa
+    const expenseCategory = document.createElement("span")
+    expenseCategory.textContent = newExpense.category_name
+
+    // Adiciona o nome e a categoria na div das informações da despesa
+    expenseInfo.append(expenseName, expenseCategory)
+
+    // Cria o valor da despesa
+    const expenseAmount = document.createElement("span")
+    expenseAmount.classList.add("expense-amount")
+    expenseAmount.innerHTML = `<small>R$</small>${newExpense.amount.toUpperCase().replace("R$", "")}`
+
+    // Cria o ícone de remover
+    const removeIcon = document.createElement("img")
+    removeIcon.classList.add("remove-icon")
+    removeIcon.setAttribute("src", "img/remove.svg")
+    removeIcon.setAttribute("alt", "Remover")
+
+    // Adiciona as informações no item
+    expenseItem.append(expenseIcon, expenseInfo, expenseAmount, removeIcon)
+
+    // Adiciona o item na lista
+    expenseList.append(expenseItem)
+    
+  } catch (error) {
+    alert('Não foi possível atualizar a lista de despesas.')
+    console.log(error)
+  }
 }
